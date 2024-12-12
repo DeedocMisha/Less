f84a4ad0780a38612041556f9a915b647f19f459
DeedocMisha <mishasvitski@gmail.com>
2024-12-12 21:29:04 +0300

    first commit

---
+++ src/components/MaIne.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="disabled">
+    <button class="btn">Disable</button><dt/><dt/>
+<b>kqdnwjhjdwwdnkjdwnkjqdnwjn</b><dt/>
+    <button class="btn">.btn+Hover</button><dt/><dt/>
+    <button class="btn--primary">.btn --primary</button><dt/><dt/>
+<button class="btn--function">Function</button><dt/><dt/>
+  </div>
+  <div class="block"></div>
+</template>
+<script setup lang="ts">
+</script>
